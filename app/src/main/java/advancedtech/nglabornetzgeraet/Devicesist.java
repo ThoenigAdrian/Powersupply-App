c5e28dee320ed0d771f5e1391dd1bd34e739294b
Adrian <thoenig.adrian@hotmail.com>
2017-06-24 18:22:02 +0200

    Implemented Power Supply List on Start Screen

--- app/src/main/java/advancedtech/nglabornetzgeraet/Devicesist.java
+++ app/src/main/java/advancedtech/nglabornetzgeraet/Devicesist.java
@@ -5,14 +5,33 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ButtonBarLayout;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class Devicesist extends AppCompatActivity {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Devicesist extends AppCompatActivity{
+
+    private ArrayList<String> data = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +40,29 @@ public class Devicesist extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ListView lv = (ListView) findViewById(R.id.listview);
+        generateListContent();
+        lv.setAdapter(new MyListAdapter(this, R.layout.power_supply_list_item, data));
 
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                Intent intent = new Intent(Devicesist.this, PowerSupplyConnecting.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+        Intent intent = new Intent(Devicesist.this, Settings.class);
+        startActivity(intent);
+    }
+
+    private void generateListContent(){
+        for(int i = 0; i < 10; i++){
+            data.add("Netzgerät ID:  " + new Random().nextInt(999999));
+        }
     }
 
     @Override
@@ -45,6 +86,7 @@ public class Devicesist extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 
     public void settingsButtonOnClick(View view) {
         Intent intent = new Intent(this, Settings.class);
@@ -90,7 +132,7 @@ public class Devicesist extends AppCompatActivity {
         return false;
     }
 
-    public void onConnectingScreenButtonClick(View view) {
+    public void onConnect(View view) {
         if(bluetoothAdapterAvailable())
             Toast.makeText(this, "Bluetooth Adapter Available", Toast.LENGTH_LONG).show();
         else
@@ -110,6 +152,48 @@ public class Devicesist extends AppCompatActivity {
 
         Intent intent = new Intent(this, PowerSupplyConnecting.class);
         startActivity(intent);
+    }
+
+    public void updateDeviceList(){
+
+    }
+
+    private class MyListAdapter extends ArrayAdapter<String> {
+        private int layout;
+        public MyListAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<String> objects) {
+            super(context, resource, objects);
+            layout = resource;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            ViewHolder mainViewHolder = null;
+
+            if(convertView == null){
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout, parent, false);
+                ViewHolder viewHolder = new ViewHolder();
+                viewHolder.powerSupplyIcon = (ImageView) convertView.findViewById(R.id.powerSupplyIcon);
+                viewHolder.powerSupplyInfo = (TextView) convertView.findViewById(R.id.powerSupplyInfo);
+                viewHolder.connectionTypeSymbol = (ImageView) convertView.findViewById(R.id.connectionTypeSymbol);
+
+                convertView.setTag(viewHolder);
+            }
+
+            else{
+                mainViewHolder = (ViewHolder) convertView.getTag();
+                mainViewHolder.powerSupplyInfo.setText(getItem(position));
+            }
+
+            return convertView;
+        }
+    }
+
+    public class ViewHolder{
+        ImageView powerSupplyIcon;
+        TextView powerSupplyInfo;
+        ImageView connectionTypeSymbol;
     }
 
     // Reading Settings
