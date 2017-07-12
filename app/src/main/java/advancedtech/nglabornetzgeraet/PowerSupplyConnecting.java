8ce7c949531fe2fbc3766f9598c08c83a2898d1f
Adrian <thoenig.adrian@hotmail.com>
2017-07-12 04:17:10 +0200

    Add asynchronus tcp connection establishment, parse power Supply Response and Display everything

--- app/src/main/java/advancedtech/nglabornetzgeraet/PowerSupplyConnecting.java
+++ app/src/main/java/advancedtech/nglabornetzgeraet/PowerSupplyConnecting.java
@@ -2,6 +2,7 @@ package advancedtech.nglabornetzgeraet;
 
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -10,15 +11,74 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class PowerSupplyConnecting extends AppCompatActivity {
 
 
     ArrayList<Button> voltageCurrentButtons = new ArrayList<>();
+    Socket powerSupplyConnection;
+    String response;
+    String ip="192.168.0.200";
+    int port=2223;
+
+    private class connectToPowerSupply extends AsyncTask<Void,Void,Void> {
+
+        private boolean error_occured=false;
+        private String error_string="";
+
+        @Override
+        protected  Void doInBackground(Void[] params) {
+            try {
+                powerSupplyConnection = new Socket(ip, port);
+            } catch (IOException e) {
+                error_occured=true;
+                error_string = e.toString();
+                return null;
+
+            } catch (Exception e) {
+                error_occured=true;
+                error_string = e.toString();
+                return null;
+            }
+
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(powerSupplyConnection.getInputStream()));
+                for (int i = 0; i < 10; i++) {
+                    response = reader.readLine();
+                    if (response == null) {
+                        error_occured = true;
+                        error_string = "couldn't read from tcp stream";
+                    }
+
+                }
+            } catch (Exception e) {
+                error_occured=true;
+                error_string = e.toString();
+                return null;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void a) {
+            if(error_occured)
+                Toast.makeText(getApplicationContext(), error_string, Toast.LENGTH_LONG).show();
+            else
+                onConnectionSuccessfull();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,14 +101,9 @@ public class PowerSupplyConnecting extends AppCompatActivity {
         voltageCurrentButtons.add(decrease_voltage_channel_1);
         voltageCurrentButtons.add(decrease_voltage_channel_2);
 
-
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-               onConnectionSuccessfull();
-            }
-        }, 2000);
+        connectToPowerSupply asyncConnectTask = new connectToPowerSupply();
+        asyncConnectTask.execute();
+        
 
     }
 
@@ -69,5 +124,31 @@ public class PowerSupplyConnecting extends AppCompatActivity {
         ((TextView) findViewById(R.id.voltage_channel_2)).setTextColor(Color.BLACK);
         ((TextView) findViewById(R.id.current_channel_1)).setTextColor(Color.BLACK);
         ((TextView) findViewById(R.id.current_channel_2)).setTextColor(Color.BLACK);
+        setAllData(response);
+
+    }
+
+    public  void setAllData(String resp)
+    {
+        try {
+            JSONObject reader = new JSONObject(resp);
+            JSONObject data = reader.getJSONObject("data");
+            EditText liveDataChannel1 = (EditText) findViewById(R.id.live_data_content_channel1);
+            EditText liveDataChannel2 = (EditText) findViewById(R.id.live_data_content_channel2);
+
+            String current = data.getString("live_current_channel_1");
+            String voltage = data.getString("live_voltage_channel_1");
+            String power = ((Float) (Float.parseFloat(voltage) * Float.parseFloat(current))).toString();
+            liveDataChannel1.setText("Leistung: " + power + "W\n" + "Spannung: " + voltage + "V\n" +
+                    "Strom: " + current + "A\n");
+
+            current = data.getString("live_current_channel_2");
+            voltage = data.getString("live_voltage_channel_2");
+            power = ((Float) (Float.parseFloat(voltage) * Float.parseFloat(current))).toString();
+            liveDataChannel2.setText("Leistung: " + power + "W\n" + "Spannung: " + voltage + "V\n" +
+                    "Strom: " + current + "A\n");
+        } catch (JSONException e) {
+            Toast.makeText(this, "Error during setAllData" + e.toString(), Toast.LENGTH_LONG).show();
+        }
     }
 }
